f9f3ec3e7414a5282f36c9a455ce2c268994d236
akholite <52768806+akholite@users.noreply.github.com>
2021-02-07 16:42:10 +0300

    Добавила задачу 3

---
+++ Zadacha3.java
@@ -0,0 +1,15 @@
+public class Zadacha3 {
+    public static void main(String[] args) {
+        int[] a = {1, 9, 15, 3, 75};
+        int x, last = a.length - 1;
+
+        x = a[0];
+        a[0] = a[last];
+        a[last] = x;
+
+        int mid = last / 2;
+        int sum = a[last] + a[mid];
+
+        System.out.println("Сумма последнего и среднего элементов: " + sum);
+    }
+}
